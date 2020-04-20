8e7b2acbeb4bf04c538c6fef599fd199c1c8ab8d
Haneesh Kesari <haneesh_kesari@brown.edu>
2020-04-19 22:55:30 -0400

    Pywren files

---
+++ Pywren/add.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+extern "C" {
+double add(double x, double y)
+{
+return 2*x+3*y;
+}
+}
---
+++ Pywren/mult.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+extern "C" {
+double mult(double x)
+{
+return 3.5*x;
+}
+}
