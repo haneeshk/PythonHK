11488727f155ec70e2cef478e967e1dd39fd2600
Haneesh Kesari <haneesh_kesari@brown.edu>
2020-05-01 00:24:02 -0400

    reg

--- Pywren/pywren_test.py
+++
@@ -1,7 +0,0 @@
-def my_function(b):
-    x = np.random.normal(0, b, 1024)
-    A = np.random.normal(0, b, (1024, 1024))
-    return np.dot(A, x)
-
-pwex = pywren.default_executor()
-res = pwex.map(my_function, np.linspace(0.1, 100, 1000))
