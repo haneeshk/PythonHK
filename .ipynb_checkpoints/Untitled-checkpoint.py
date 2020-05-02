e2fe9a87850b6e3e400e48e3e2ab3e6931ca9b86
Haneesh Kesari <haneesh_kesari@brown.edu>
2020-05-02 15:37:47 -0400

    reg

---
+++ .ipynb_checkpoints/Untitled-checkpoint.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## 
+
+# In[ ]:
+
+
+get_ipython().run_cell_magic('latex', '', '\n$$\\newcommand{\\u}[1]{\\boldsymbol{#1}}$$\n    \n    \n$\\u{x}$    \n    \n\\begin{equation}\nx=2\n\\end{equation}')
+
+
+# In[ ]:
+
+
+get_ipython().run_cell_magic('latex', '', '\n$\\u{x}$')
+
