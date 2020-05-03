6dc6c13a98f80b1d9a91c4644b1c17980ebd986e
Haneesh Kesari <haneesh_kesari@brown.edu>
2020-05-03 16:24:42 -0400

    Python Pushing

--- startup/initHK.py
+++ startup/initHK.py
@@ -9,3 +9,13 @@ from funcoperators import postfix as to, infix, compose as ov # composition oper
 def at(func, arg):                          # evaluate at operator
     return list(map(func, [arg]))[0]
     return list(map(func, [arg]))[0]
+
+
+
+
+
+
+if __name__ == '__main__':
+    # test1.py executed as script
+    # do something
+    at()
