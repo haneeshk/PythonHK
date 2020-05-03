636646067fd4fd2337ed50ec33fdf0b2c896744b
Haneesh Kesari <haneesh_kesari@brown.edu>
2020-05-03 15:13:46 -0400

    Python Pushing

---
+++ Untitled.py
@@ -0,0 +1,33 @@
+# %%
+"""
+##
+
+[Start Up script](file://Users/haneeshkesari/.ipython/profile_default/startup/00_stratup.py)
+"""
+
+# %%
+%%latex 
+
+$$\newcommand{\u}[1]{\boldsymbol{#1}}$$
+    
+    
+$\u{x}$    
+    
+  
+  
+  
+\begin{equation}
+x=2
+\end{equation}
+
+# %%
+get_ipython().run_cell_magic('latex', '','\n$$\\newcommand{\\u}[1]{\\boldsymbol{#1}}$$\n$$\newcommand{\forcemag}{f}$$\n\n$$\newcommand{\physF}{\boldsymbol{\mathfrak{f}}} %$$\n\n$$\newcommand{\physB}{\mathscr{b}} % Width of the thin film$$\n\n$$\newcommand{\physE}{{\mathpzc{E}}}$$\n\n$$\newcommand{\physh}{\mathscr{h}}$$\n\n$$\newcommand{\physe}{\mathbscr{e}}$$\n\n$$\newcommand{\physf}{\boldsymbol{\mathfrak{f}}}$$\n\n$$\newcommand{\physm}{\mathscr{m}} %$$\n\n$$\newcommand{\physu}{\mathbscr{u}}$$\n\n$$\newcommand{\physw}{\mathscr{w}}$$\n\n$$\newcommand{\rtple}[1]{\boldsymbol{#1}}$$\n\renewcommand{\u}[1]{\boldsymbol{#1}}
+\n$$\newcommand{\OriginRefEucldPtSpace}{O_{\mathrm{ R}}}$$\n\n$$\newcommand{\MapManifoldToRefEucldPtSpace}{\kappa_{\mathrm{ R}}}$$\n\renewcommand{\t}[1]{\tilde{#1}}
+\renewcommand{\b}[1]{\mathbb{#1}}
+\renewcommand{\c}[1]{\mathcal{#1}}
+\n$$\newcommand{\lsc}[2][\mathscr{l}]{{}^{ #1 }\! #2}$$\n\n$$\newcommand{\lscH}[1]{\lsc[H]{#1}}$$\n\n$$\newcommand{\dsf}[1]{\Delta\boldsymbol{\sf #1}}$$\n\n$$\newcommand{\tpsb}[1]{\left. #1 \right.^{\sf T}}$$\n\n$$\newcommand{\tps}[1]{\left( #1 \right)^{\sf T}}$$\n\n$$\newcommand{\usf}[1]{\u{\sf #1}}$$\n\n$$\newcommand{\busf}[1]{\bar{\usf{ #1}}}$$\n\n$$\newcommand{\tu}[1]{\tilde{\u{ #1}}}$$\n\n$$\newcommand{\tusf}[1]{\tilde{\usf{ #1}}}$$\n\n$$\newcommand{\btusf}[1]{\bar{\tusf{ #1}}}$$\n\n$$\newcommand{\pr}[1]{\left( #1 \right)}$$\n\n$$\newcommand*{\subfigurewdith}{0.8\textwidth}$$\n\n$$\newcommand{\D}[1]{D\hspace{-.1em}#1}$$\n\n$$\newcommand{\Dr}{D\hspace{-.1em}\varrho}$$\n\n$$\newcommand{\Drp}{\Dr^{+}}$$\n\n$$\newcommand{\Drm}{\Dr^{-}}$$\n\n$$\newcommand{\Dro}{\dot{\rho}}$$\n\n$$\newcommand{\DDro}{\ddot{\rho}}$$\n\n$$\newcommand{\Df}{\dot{f}}$$\n\n$$\newcommand{\DDf}{\ddot{f}}$$\n\n$$\newcommand{\Drop}{\Dro^{+}}$$\n\n$$\newcommand{\Drom}{\Dro^{-}}$$\n\n$$\newcommand{\Dl}{\dot{l}}$$\n\n$$\newcommand{\Da}{\dot{a}}$$\n\n$$\newcommand{\DDl}{\ddot{l}}$$\n\n$$\newcommand{\DF}{\dot{F}}$$\n\n$$\newcommand{\DDF}{\ddot{F}}$$\n\n$$\newcommand{\Dlp}{\Dl^{+}}$$\n\n$$\newcommand{\Dlm}{\Dl^{-}}$$\n\n$$\newcommand{\Dam}{\Da^{-}}$$\n\n$$\newcommand{\Deq}{\mathcal{D}^{\circ}}$$\n\n$$\newcommand{\Dst}{\mathcal{D}^{\stable}}$$\n\n$$\newcommand{\Dnt}{\mathcal{D}^{\odot}}$$\n\n$$\newcommand{\Dunst}{\mathcal{D}^{\otimes}}$$\n\n$$\newcommand{\conf}{\boldsymbol{\kappa}}$$\n\n$$\newcommand{\confp}{\tilde{\boldsymbol{\kappa}}}$$\n')
+
+# %%
+%%latex 
+
+$\u{x}$
