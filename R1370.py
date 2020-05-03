a3a93ad48920ad83dc5cb653c54773bfb28388cd
Haneesh Kesari <haneesh_kesari@brown.edu>
2020-05-03 19:07:12 -0400

    Python Pushing

---
+++ R1370.py
@@ -0,0 +1,16 @@
+# %% codecell
+
+import sys
+import os
+from os.path import dirname
+sys.path.append(os.getcwd()+"/startup")
+from IPython.display import display as Echo
+from initHK import *
+
+# %% markdown
+
+$$\usf{x}(\usf{X},\tau)$$
+
+# %%latex
+
+I am good
