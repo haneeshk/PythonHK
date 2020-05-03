137b6c84d0db988aaf695d55df04472a59390efd
Haneesh Kesari <haneesh_kesari@brown.edu>
2020-05-03 15:02:30 -0400

    Python Pushing

---
+++ HydrogenDemo.py
@@ -0,0 +1,85 @@
+# %% codecell
+!ls
+
+# %%
+%%timeit
+list-at-range(50000)
+for i in list-at-range(10):
+    sin*ov*cos-at-(i*pi/2)
+
+
+
+
+
+# %% markdown
+
+
+The idea is that $\us
+# %% codecell
+sin*ov*cos-at-(pi/2)
+# %% markdown
+$$
+\newcommand{\forcemag}{f}
+\newcommand{\physF}{\boldsymbol{\mathfrak{f}}}
+\newcommand{\physB}{\mathscr{b}} % Width of the thin film
+\newcommand{\physE}{{\mathpzc{E}}} % Young's modulus of the thin film
+\newcommand{\physh}{\mathscr{h}} % Thickness of the thin film.
+\newcommand{\physe}{\mathbscr{e}} % Thickness of the thin film.
+\newcommand{\physf}{\boldsymbol{\mathfrak{f}}} % Thickness of the thin film.
+\newcommand{\physm}{\mathscr{m}}
+\newcommand{\physu}{\mathbscr{u}}
+\newcommand{\physw}{\mathscr{w}}
+\newcommand{\rtple}[1]{\boldsymbol{#1}}
+\renewcommand{\u}[1]{\boldsymbol{#1}}
+\newcommand{\OriginRefEucldPtSpace}{O_{\mathrm{ R}}}
+\newcommand{\MapManifoldToRefEucldPtSpace}{\kappa_{\mathrm{ R}}}
+\renewcommand{\t}[1]{\tilde{#1}}
+\renewcommand{\b}[1]{\mathbb{#1}}
+\renewcommand{\c}[1]{\mathcal{#1}}
+\newcommand{\lsc}[2][\mathscr{l}]{{}^{ #1 }\! #2}
+\newcommand{\lscH}[1]{\lsc[H]{#1}}
+\newcommand{\dsf}[1]{\Delta\boldsymbol{\sf #1}}
+\newcommand{\tpsb}[1]{\left. #1 \right.^{\sf T}}
+\newcommand{\tps}[1]{\left( #1 \right)^{\sf T}}
+\newcommand{\usf}[1]{\u{\sf #1}}
+\newcommand{\busf}[1]{\bar{\usf{ #1}}}
+\newcommand{\tu}[1]{\tilde{\u{ #1}}}
+\newcommand{\tusf}[1]{\tilde{\usf{ #1}}}
+\newcommand{\btusf}[1]{\bar{\tusf{ #1}}}
+\newcommand{\pr}[1]{\left( #1 \right)}
+\newcommand{\D}[1]{D\hspace{-.1em}#1}
+\newcommand{\Dr}{D\hspace{-.1em}\varrho}
+\newcommand{\Drp}{\Dr^{+}}
+\newcommand{\Drm}{\Dr^{-}}
+\newcommand{\Dro}{\dot{\rho}}
+\newcommand{\DDro}{\ddot{\rho}}
+\newcommand{\Df}{\dot{f}}
+\newcommand{\DDf}{\ddot{f}}
+\newcommand{\Drop}{\Dro^{+}}
+\newcommand{\Drom}{\Dro^{-}}
+\newcommand{\Dl}{\dot{l}}
+\newcommand{\Da}{\dot{a}}
+\newcommand{\DDl}{\ddot{l}}
+\newcommand{\DF}{\dot{F}}
+\newcommand{\DDF}{\ddot{F}}
+\newcommand{\Dlp}{\Dl^{+}}
+\newcommand{\Dlm}{\Dl^{-}}
+\newcommand{\Dam}{\Da^{-}}
+\newcommand{\Deq}{\mathcal{D}^{\circ}}
+\newcommand{\Dst}{\mathcal{D}^{\stable}}
+\newcommand{\Dnt}{\mathcal{D}^{\odot}}
+\newcommand{\Dunst}{\mathcal{D}^{\otimes}}
+\newcommand{\conf}{\boldsymbol{\kappa}}
+\newcommand{\confp}{\tilde{\boldsymbol{\kappa}}}$$
+
+# %% markdown
+
+
+$
+\epsilon_{\rm max}= \{ \epsilon{\usf{x}}~|~x\in \mathcal{B}\}
+$
+# %% codecell
+sin(pi)-to*at*print
+# %% codecell
+sin*at*[0,pi/4,pi/2]-to*at*print;
+# %% codecell
