edcf17f546939b2c89bafa1d43d4058e1d095c0f
Haneesh Kesari <haneesh_kesari@brown.edu>
2020-10-18 14:08:18 -0400

    Python Pushing

---
+++ MasterToHK.sh
@@ -0,0 +1,22 @@
+# Oct 18, 2020
+I like technology, engineering, and science.
+
+# Oct 17, 2020
+ls
+echo ${HOME}
+project://HK_writeup.lyx
+# this is some long test
+echo $HOME | pbcopy
+open /Users/haneeshkesari/June23_2020.rtf	-a TextEdit
+open http://yahoo.com
+
+[link](./index.ts)
+link
+file://
+ls ~
+[Alice Sqrt 1](file:///Users/haneeshkesari/Downloads/HackMD_User_1602092220923/Introduction_%20$_sqrt%7BAO%7D$%20manuscript.md)
+//file:///Users/haneeshkesari/Downloads/HackMD_User_1602092220923/Introduction_%20$_sqrt%7BAO%7D$%20manuscript.md
+
+
+
+open http://www.google.com # Google
