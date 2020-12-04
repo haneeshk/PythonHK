8c30dd997d6b9c2a2da0334c592a92ab662ab198
Haneesh Kesari <haneesh_kesari@brown.edu>
2020-12-04 01:09:29 -0500

    Python Pushing

--- const.py
+++ const.py
@@ -1,10 +1,45 @@
+
+
+X_1=1
+
+x_1=(i for i in range(3))
+x_1=tuple(x_1)
+x_1
+ᴬx=1
+ᴬx
+ᴬxₐ=2
+ᴬxₐ
+ᵃxₐ=2
+ᵃx𝔑=3
+xᵇ=3
+xᵤ=2
+[ ᵃxᵤ for u in
+=22.0
+ᵃxᵤ=1
+ᵃxᵥ=2
+ᵃxₓ=3
+
+ᵃxᵤ+ᵃxᵥ+ᵃxₓ
+
+
+x=[[j+i for i in range(3)] for j in range(3)]
+x
+
+x[2][2]
+
+math.sin(ᵃxᵤ)
+x_1[0]
+x_1
+
+unicode_character(2096)
+
 MatConstants=(("Young's Modulus",13.3,"KPa"),("pi",3.14,"Non-dimensional"))
 MatConstantsKeys=( (i,entry[0]) for i,entry in enumerate(MatConstants))
 MatConstantsKeys
 MatConstantsKeys=tuple(MatConstantsKeys)
 MatConstantsKeys
-
-
+txt=u'\x00\x00\x00'
+txt.encode("utf-8"
 class YoungsModulus():
     __value__=0.0
     __units__="KPa"
@@ -14,12 +49,18 @@ class YoungsModulus():
     def value(self):
         return self.__value__
 
-
+x² = 3
+for i in range(1100,2000):
+    print(i, chr(i))
+ Ϭ=2
+  Ϭ**2
+ݽ =2
+ݽ*8=
 YM=YoungsModulus(10,"MPa")
 YM2=YoungsModulus(10)
 
 def CreateConstant(Value,Unit="ND"):
-    return lambda x=0 :  Unit if x is "units" else Value
+    return lambda x=0 :  Unit if x is "units" else Valuerest
 
 ν=CreateConstant(0.3)
 E=CreateConstant(220,"KPa")
@@ -30,7 +71,15 @@ E=CreateConstant(220,"KPa")
 ν("Value")*E("Value")
 E("value")
 
-
+unicode_range = ('4E00', '9FFF')  # (CJK Unified Ideographs)
+characters = []
+for unicode_character in range(int(unicode_range[0], 16), int(unicode_range[1], 16)):
+    characters.append(chr(unicode_character))
+for c in characters:
+    print(c)
+[3]
+丂=2
+math.sin(丂)**2
 E=22.0
 
 E()=33
