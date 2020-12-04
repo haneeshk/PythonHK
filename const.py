cb5067a62340383377d0df298eb6309c57a89f1c
Haneesh Kesari <haneesh_kesari@brown.edu>
2020-12-03 21:22:39 -0500

    Python Pushing

--- const.py
+++ const.py
@@ -1,9 +1,24 @@
-# Put in const.py...:
-class _const:
-    class ConstError(TypeError): pass
-    def __setattr__(self,name,value):
-        if self.__dict__.has_key(name):
-            raise self.ConstError
-        self.__dict__[name]=value
-import sys
-sys.modules[__name__]=_const()
+MatConstants=(("Young's Modulus",13.3,"KPa"),("pi",3.14,"Non-dimensional"))
+MatConstantsKeys=( (i,entry[0]) for i,entry in enumerate(MatConstants))
+MatConstantsKeys
+MatConstantsKeys=tuple(MatConstantsKeys)
+MatConstantsKeys
+
+
+class YoungsModulus():
+    __value__
+    __units__
+    def __init__(self,value,units="KPa"):
+        __value__=value
+        __units__=units
+
+
+
+
+from IPython.display import display as Echo
+from numpy import *
+import numpy as np
+from funcoperators import postfix as to, infix, compose as ov # composition operator
+@infix
+def at(func, arg):                          # evaluate at operator
+    return list(map(func,[arg]))[0]
